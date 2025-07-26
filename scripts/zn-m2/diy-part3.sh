e8b0dd545c8bf620fe49b29d0afa0442fd9be7b8
kkstone <10545609+kkstone@users.noreply.github.com>
2025-07-26 09:18:59 +0800

    Update diy-part3.sh

--- scripts/zn-m2/diy-part3.sh
+++ scripts/zn-m2/diy-part3.sh
@@ -13,8 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-# design修改proxy链接
-sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
-
 rm -rf $GITHUB_WORKSPACE/openwrt/package/system/ca-certificates/Makefile
-cp -rf $GITHUB_WORKSPACE/patchs/ca-Makefile $GITHUB_WORKSPACE/openwrt/package/system/ca-certificates/Makefile
+cp -rf $GITHUB_WORKSPACE/patches/ca-Makefile $GITHUB_WORKSPACE/openwrt/package/system/ca-certificates/Makefile
