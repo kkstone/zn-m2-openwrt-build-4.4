0ec858429e4ebae4aabdaef672a2699a679bf119
kkstone <10545609+kkstone@users.noreply.github.com>
2025-07-26 08:39:36 +0800

    Update diy-part3.sh

--- scripts/zn-m2/diy-part3.sh
+++ scripts/zn-m2/diy-part3.sh
@@ -13,11 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-#修改版本信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
-# 替换golang版本为1.24
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
 # design修改proxy链接
 sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
