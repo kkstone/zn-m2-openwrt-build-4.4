edc27438d7c101e9a5237398a367944474d4511d
kkstone <10545609+kkstone@users.noreply.github.com>
2025-07-26 08:54:34 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -12,3 +12,15 @@
 
 # Uncomment a feed source
 #echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+
+cd package
+git clone https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git && rm -rf openwrt-passwall-packages/tcping
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/kuoruan/openwrt-frp.git -b releases/v0.63.0-1
+git clone https://github.com/mwarning/zerotier-openwrt.git && rm -rf zerotier-openwrt/zerotier/files/etc/init.d/zerotier
+git clone https://github.com/kkstone/7zz-openwrt.git
+
+unzip -d ./ $GITHUB_WORKSPACE/luci-packages/luci-app-frp.zip
+unzip -d ./ $GITHUB_WORKSPACE/luci-packages/luci-app-wolplus.zip
