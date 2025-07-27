950fed23b38a7250705773a6d0d495b734b2472b
kkstone <10545609+kkstone@users.noreply.github.com>
2025-07-27 08:04:01 +0800

    Update diy-part3.sh

--- scripts/zn-m2/diy-part3.sh
+++ scripts/zn-m2/diy-part3.sh
@@ -15,14 +15,19 @@
 
 rm -rf $GITHUB_WORKSPACE/openwrt/package/system/ca-certificates/Makefile
 cp -rf $GITHUB_WORKSPACE/patches/ca-Makefile $GITHUB_WORKSPACE/openwrt/package/system/ca-certificates/Makefile
-rm -rf $GITHUB_WORKSPACE/openwrt/package/network/utils/iwinfo/Makefile
-wget -O $GITHUB_WORKSPACE/openwrt/package/network/utils/iwinfo/Makefile https://github.com/immortalwrt/immortalwrt/raw/refs/heads/openwrt-21.02/package/network/utils/iwinfo/Makefile
+#rm -rf $GITHUB_WORKSPACE/openwrt/package/network/utils/iwinfo/Makefile
+#wget -O $GITHUB_WORKSPACE/openwrt/package/network/utils/iwinfo/Makefile https://github.com/immortalwrt/immortalwrt/raw/refs/heads/openwrt-21.02/package/network/utils/iwinfo/Makefile
 
 rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/net/ksmbd-tools/Makefile
 cp -rf $GITHUB_WORKSPACE/patches/ksmbd-tools-Makefile $GITHUB_WORKSPACE/openwrt/feeds/packages/net/ksmbd-tools/Makefile
 rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/Makefile
 cp -rf $GITHUB_WORKSPACE/patches/ksmbd-Makefile $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/Makefile
 rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/patches/02-fix_zdi_22_1690.patch
+
+cp -rf $GITHUB_WORKSPACE/patches/luci-openwrt-21.02.zip $GITHUB_WORKSPACE/openwrt/feeds/luci-openwrt-21.02.zip
+rm -rf $GITHUB_WORKSPACE/openwrt/feeds/luci/applications
+unzip $GITHUB_WORKSPACE/openwrt/feeds/luci-openwrt-21.02.zip && rm -rf $GITHUB_WORKSPACE/openwrt/feeds/luci-openwrt-21.02.zip
+rm -rf $GITHUB_WORKSPACE/openwrt/feeds/luci/themes/{luci-theme-B*,luci-theme-a*,luci-theme-bootstrap-mod,luci-theme-d*,luci-theme-i*,luci-theme-m*,luci-theme-n*,luci-theme-o*,luci-theme-r*}
 
 echo 'CONFIG_NFSD_V4=y' >> $GITHUB_WORKSPACE/openwrt/target/linux/ipq60xx/config-4.4
 echo 'CONFIG_NFSD_PNFS=y' >> $GITHUB_WORKSPACE/openwrt/target/linux/ipq60xx/config-4.4
