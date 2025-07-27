2e431c5bad3de9bbb264b40d91eadf67e1b7915f
kkstone <10545609+kkstone@users.noreply.github.com>
2025-07-27 14:12:01 +0800

    Update diy-part3.sh

--- scripts/zn-m2/diy-part3.sh
+++ scripts/zn-m2/diy-part3.sh
@@ -18,23 +18,23 @@ cp -rf $GITHUB_WORKSPACE/patches/ca-Makefile $GITHUB_WORKSPACE/openwrt/package/s
 #rm -rf $GITHUB_WORKSPACE/openwrt/package/network/utils/iwinfo/Makefile
 #wget -O $GITHUB_WORKSPACE/openwrt/package/network/utils/iwinfo/Makefile https://github.com/immortalwrt/immortalwrt/raw/refs/heads/openwrt-21.02/package/network/utils/iwinfo/Makefile
 
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/net/ksmbd-tools/Makefile
-cp -rf $GITHUB_WORKSPACE/patches/ksmbd-tools-Makefile $GITHUB_WORKSPACE/openwrt/feeds/packages/net/ksmbd-tools/Makefile
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/Makefile
-cp -rf $GITHUB_WORKSPACE/patches/ksmbd-Makefile $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/Makefile
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/patches/02-fix_zdi_22_1690.patch
+#rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/net/ksmbd-tools/Makefile
+#cp -rf $GITHUB_WORKSPACE/patches/ksmbd-tools-Makefile $GITHUB_WORKSPACE/openwrt/feeds/packages/net/ksmbd-tools/Makefile
+#rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/Makefile
+#cp -rf $GITHUB_WORKSPACE/patches/ksmbd-Makefile $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/Makefile
+#rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/kernel/ksmbd/patches/02-fix_zdi_22_1690.patch
 
-mkdir $GITHUB_WORKSPACE/openwrt/tmp_tmp
-cd $GITHUB_WORKSPACE/openwrt/tmp_tmp
-wget https://github.com/immortalwrt/luci/archive/refs/heads/openwrt-21.02.zip
-unzip openwrt-21.02.zip
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/*
-rm -rf luci-openwrt-21.02/applications/luci-app-firewall
-mv luci-openwrt-21.02/applications/* $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/*
-wget https://github.com/openwrt-fork/sdf8057-luci/archive/refs/heads/2023.zip
-unzip 2023.zip
-mv sdf8057-luci-2023/applications/luci-app-firewall $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-firewall
-cd ~ && rm -rf $GITHUB_WORKSPACE/openwrt/tmp_tmp
+#mkdir $GITHUB_WORKSPACE/openwrt/tmp_tmp
+#cd $GITHUB_WORKSPACE/openwrt/tmp_tmp
+#wget https://github.com/immortalwrt/luci/archive/refs/heads/openwrt-21.02.zip
+#unzip openwrt-21.02.zip
+#rm -rf $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/*
+#rm -rf luci-openwrt-21.02/applications/luci-app-firewall
+#mv luci-openwrt-21.02/applications/* $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/*
+#wget https://github.com/openwrt-fork/sdf8057-luci/archive/refs/heads/2023.zip
+#unzip 2023.zip
+#mv sdf8057-luci-2023/applications/luci-app-firewall $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-firewall
+#cd ~ && rm -rf $GITHUB_WORKSPACE/openwrt/tmp_tmp
 
 echo 'CONFIG_NFSD_V4=y' >> $GITHUB_WORKSPACE/openwrt/target/linux/ipq60xx/config-4.4
 echo 'CONFIG_NFSD_PNFS=y' >> $GITHUB_WORKSPACE/openwrt/target/linux/ipq60xx/config-4.4
